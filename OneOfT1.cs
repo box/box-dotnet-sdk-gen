c2c8ce3a54744ff02e5799f546582658e7d9ce68
box-sdk-build <sdk-build@box.com>
2023-08-31 05:18:05 -0700

    generated with codegen at box/box-codegen@d8b234c and spec at box/box-openapi@9e5ee33

--- OneOfT1.cs
+++
@@ -1,9 +0,0 @@
-namespace Unions {
-    public class OneOf<T0> {
-        protected T0? _val0 { get; }
-        
-        protected OneOf (T0 value) { _val0 = value; }
-        
-        public static implicit operator OneOf<T0>(T0 value) => new OneOf<T0>(value);
-    }
-}
