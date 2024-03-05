cdc5806fba30cf07e4f17e02e106b55c0a7c6a1d
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-03-05 10:57:15 +0100

    feat: Support version in request headers (box/box-codegen#418) (#34)

--- Box.Sdk.Gen/Networking/SdkConsts.cs
+++ Box.Sdk.Gen/Networking/SdkConsts.cs
@@ -4,10 +4,9 @@ namespace Fetch
 {
     internal static class SdkConsts
     {
-        static string SdkVersion = "0.1.0";
         static string RunTimeVersion = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
 
-        static KeyValuePair<string, string> UserAgentHeader = new KeyValuePair<string, string>("User-Agent", $"box-dotnet-generated-sdk/{SdkVersion}");
+        static KeyValuePair<string, string> UserAgentHeader = new KeyValuePair<string, string>("User-Agent", $"box-dotnet-generated-sdk/{Version.sdkVersion}");
         static KeyValuePair<string, string> BoxUAHeader = new KeyValuePair<string, string>("X-Box-UA", $"agent={UserAgentHeader.Value}; env=dotnet/{RunTimeVersion}");
 
         internal static List<KeyValuePair<string, string>> AnalyticsHeaders = new List<KeyValuePair<string, string>>() { UserAgentHeader, BoxUAHeader };
