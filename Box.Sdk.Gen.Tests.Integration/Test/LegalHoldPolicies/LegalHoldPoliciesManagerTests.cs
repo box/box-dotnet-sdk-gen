96745b64b75ded429a3ae5709ea826ffddede406
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-03-22 13:19:25 +0100

    fix: fix `datetimeToString` utils function (box/box-codegen#459) (#57)

--- Box.Sdk.Gen.Tests.Integration/Test/LegalHoldPolicies/LegalHoldPoliciesManagerTests.cs
+++ Box.Sdk.Gen.Tests.Integration/Test/LegalHoldPolicies/LegalHoldPoliciesManagerTests.cs
@@ -30,5 +30,17 @@ namespace Box.Sdk.Gen.Tests.Integration {
             await client.LegalHoldPolicies.DeleteLegalHoldPolicyByIdAsync(legalHoldPolicyId: legalHoldPolicyId);
         }
 
+        [TestMethod]
+        public async System.Threading.Tasks.Task TestCreateNotOngoingLegalHoldPolicy() {
+            string legalHoldPolicyName = Utils.GetUUID();
+            const string legalHoldDescription = "test description";
+            LegalHoldPolicy legalHoldPolicy = await client.LegalHoldPolicies.CreateLegalHoldPolicyAsync(requestBody: new CreateLegalHoldPolicyRequestBody(policyName: legalHoldPolicyName) { Description = legalHoldDescription, IsOngoing = false, FilterStartedAt = Utils.DateTimeFromString(dateTime: "2021-01-01T00:00:00-08:00"), FilterEndedAt = Utils.DateTimeFromString(dateTime: "2022-01-01T00:00:00-08:00") });
+            Assert.IsTrue(legalHoldPolicy.PolicyName == legalHoldPolicyName);
+            Assert.IsTrue(legalHoldPolicy.Description == legalHoldDescription);
+            Assert.IsTrue(Utils.DateTimeToString(dateTime: NullableUtils.Unwrap(legalHoldPolicy.FilterStartedAt)) == "2021-01-01T00:00:00-08:00");
+            Assert.IsTrue(Utils.DateTimeToString(dateTime: NullableUtils.Unwrap(legalHoldPolicy.FilterEndedAt)) == "2022-01-01T00:00:00-08:00");
+            await client.LegalHoldPolicies.DeleteLegalHoldPolicyByIdAsync(legalHoldPolicyId: legalHoldPolicy.Id);
+        }
+
     }
 }
