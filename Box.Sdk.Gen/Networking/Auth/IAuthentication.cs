6fc389518f3a479d2e9f686b4c08ee640d40afb9
box-sdk-build <sdk-build@box.com>
2024-04-24 09:40:03 -0700

    feat: Generate Auth for Swift (box/box-codegen#469)

--- Box.Sdk.Gen/Networking/Auth/IAuthentication.cs
+++ Box.Sdk.Gen/Networking/Auth/IAuthentication.cs
@@ -1,7 +1,4 @@
 using Unions;
-using System;
-using System.Collections.ObjectModel;
-using System.Collections.Generic;
 using Box.Sdk.Gen.Schemas;
 using Box.Sdk.Gen;
 
@@ -12,10 +9,6 @@ namespace Box.Sdk.Gen {
         public System.Threading.Tasks.Task<AccessToken> RefreshTokenAsync(NetworkSession? networkSession = null);
 
         public System.Threading.Tasks.Task<string> RetrieveAuthorizationHeaderAsync(NetworkSession? networkSession = null);
-
-        public System.Threading.Tasks.Task RevokeTokenAsync(NetworkSession? networkSession = null);
-
-        public System.Threading.Tasks.Task<AccessToken> DownscopeTokenAsync(IReadOnlyList<string> scopes, string? resource = null, string? sharedLink = null, NetworkSession? networkSession = null);
 
     }
 }
