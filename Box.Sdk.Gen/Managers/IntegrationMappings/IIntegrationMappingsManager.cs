0946d5080260a63bd9cd53412dab94892f7b7617
box-sdk-build <sdk-build@box.com>
2025-03-12 07:42:54 -0700

    feat: add Integration Mappings Teams API (box/box-openapi#517)

--- Box.Sdk.Gen/Managers/IntegrationMappings/IIntegrationMappingsManager.cs
+++ Box.Sdk.Gen/Managers/IntegrationMappings/IIntegrationMappingsManager.cs
@@ -83,5 +83,76 @@ namespace Box.Sdk.Gen.Managers {
     /// </param>
     public System.Threading.Tasks.Task DeleteSlackIntegrationMappingByIdAsync(string integrationMappingId, DeleteSlackIntegrationMappingByIdHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) => throw new System.NotImplementedException("This method needs to be implemented by the derived class before calling it.");
 
+        /// <summary>
+    /// Lists [Teams integration mappings](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams) in a users' enterprise.
+    /// You need Admin or Co-Admin role to
+    /// use this endpoint.
+    /// </summary>
+    /// <param name="queryParams">
+    /// Query parameters of getIntegrationMappingTeams method
+    /// </param>
+    /// <param name="headers">
+    /// Headers of getIntegrationMappingTeams method
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token used for request cancellation.
+    /// </param>
+    public System.Threading.Tasks.Task<IntegrationMappingsTeams> GetIntegrationMappingTeamsAsync(GetIntegrationMappingTeamsQueryParams? queryParams = default, GetIntegrationMappingTeamsHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) => throw new System.NotImplementedException("This method needs to be implemented by the derived class before calling it.");
+
+        /// <summary>
+    /// Creates a [Teams integration mapping](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams)
+    /// by mapping a Teams channel to a Box item.
+    /// You need Admin or Co-Admin role to
+    /// use this endpoint.
+    /// </summary>
+    /// <param name="requestBody">
+    /// Request body of createIntegrationMappingTeams method
+    /// </param>
+    /// <param name="headers">
+    /// Headers of createIntegrationMappingTeams method
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token used for request cancellation.
+    /// </param>
+    public System.Threading.Tasks.Task<IntegrationMappingTeams> CreateIntegrationMappingTeamsAsync(IntegrationMappingTeamsCreateRequest requestBody, CreateIntegrationMappingTeamsHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) => throw new System.NotImplementedException("This method needs to be implemented by the derived class before calling it.");
+
+        /// <summary>
+    /// Updates a [Teams integration mapping](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams).
+    /// Supports updating the Box folder ID and options.
+    /// You need Admin or Co-Admin role to
+    /// use this endpoint.
+    /// </summary>
+    /// <param name="integrationMappingId">
+    /// An ID of an integration mapping
+    /// Example: "11235432"
+    /// </param>
+    /// <param name="requestBody">
+    /// Request body of updateIntegrationMappingTeamsById method
+    /// </param>
+    /// <param name="headers">
+    /// Headers of updateIntegrationMappingTeamsById method
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token used for request cancellation.
+    /// </param>
+    public System.Threading.Tasks.Task<IntegrationMappingTeams> UpdateIntegrationMappingTeamsByIdAsync(string integrationMappingId, UpdateIntegrationMappingTeamsByIdRequestBody? requestBody = default, UpdateIntegrationMappingTeamsByIdHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) => throw new System.NotImplementedException("This method needs to be implemented by the derived class before calling it.");
+
+        /// <summary>
+    /// Deletes a [Teams integration mapping](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams).
+    /// You need Admin or Co-Admin role to
+    /// use this endpoint.
+    /// </summary>
+    /// <param name="integrationMappingId">
+    /// An ID of an integration mapping
+    /// Example: "11235432"
+    /// </param>
+    /// <param name="headers">
+    /// Headers of deleteIntegrationMappingTeamsById method
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token used for request cancellation.
+    /// </param>
+    public System.Threading.Tasks.Task DeleteIntegrationMappingTeamsByIdAsync(string integrationMappingId, DeleteIntegrationMappingTeamsByIdHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) => throw new System.NotImplementedException("This method needs to be implemented by the derived class before calling it.");
+
     }
 }
--- Box.Sdk.Gen/Managers/IntegrationMappings/IntegrationMappingsManager.cs
+++ Box.Sdk.Gen/Managers/IntegrationMappings/IntegrationMappingsManager.cs
@@ -112,5 +112,98 @@ namespace Box.Sdk.Gen.Managers {
             FetchResponse response = await this.NetworkSession.NetworkClient.FetchAsync(options: new FetchOptions(url: string.Concat(this.NetworkSession.BaseUrls.BaseUrl, "/2.0/integration_mappings/slack/", StringUtils.ToStringRepresentation(integrationMappingId)), method: "DELETE", responseFormat: Box.Sdk.Gen.ResponseFormat.NoContent) { Headers = headersMap, Auth = this.Auth, NetworkSession = this.NetworkSession, CancellationToken = cancellationToken }).ConfigureAwait(false);
         }
 
+        /// <summary>
+        /// Lists [Teams integration mappings](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams) in a users' enterprise.
+        /// You need Admin or Co-Admin role to
+        /// use this endpoint.
+        /// </summary>
+        /// <param name="queryParams">
+        /// Query parameters of getIntegrationMappingTeams method
+        /// </param>
+        /// <param name="headers">
+        /// Headers of getIntegrationMappingTeams method
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Token used for request cancellation.
+        /// </param>
+        public async System.Threading.Tasks.Task<IntegrationMappingsTeams> GetIntegrationMappingTeamsAsync(GetIntegrationMappingTeamsQueryParams? queryParams = default, GetIntegrationMappingTeamsHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) {
+            queryParams = queryParams ?? new GetIntegrationMappingTeamsQueryParams();
+            headers = headers ?? new GetIntegrationMappingTeamsHeaders();
+            Dictionary<string, string> queryParamsMap = Utils.PrepareParams(map: new Dictionary<string, string?>() { { "partner_item_type", StringUtils.ToStringRepresentation(queryParams.PartnerItemType) }, { "partner_item_id", StringUtils.ToStringRepresentation(queryParams.PartnerItemId) }, { "box_item_id", StringUtils.ToStringRepresentation(queryParams.BoxItemId) }, { "box_item_type", StringUtils.ToStringRepresentation(queryParams.BoxItemType) } });
+            Dictionary<string, string> headersMap = Utils.PrepareParams(map: DictionaryUtils.MergeDictionaries(new Dictionary<string, string?>() {  }, headers.ExtraHeaders));
+            FetchResponse response = await this.NetworkSession.NetworkClient.FetchAsync(options: new FetchOptions(url: string.Concat(this.NetworkSession.BaseUrls.BaseUrl, "/2.0/integration_mappings/teams"), method: "GET", responseFormat: Box.Sdk.Gen.ResponseFormat.Json) { Parameters = queryParamsMap, Headers = headersMap, Auth = this.Auth, NetworkSession = this.NetworkSession, CancellationToken = cancellationToken }).ConfigureAwait(false);
+            return SimpleJsonSerializer.Deserialize<IntegrationMappingsTeams>(NullableUtils.Unwrap(response.Data));
+        }
+
+        /// <summary>
+        /// Creates a [Teams integration mapping](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams)
+        /// by mapping a Teams channel to a Box item.
+        /// You need Admin or Co-Admin role to
+        /// use this endpoint.
+        /// </summary>
+        /// <param name="requestBody">
+        /// Request body of createIntegrationMappingTeams method
+        /// </param>
+        /// <param name="headers">
+        /// Headers of createIntegrationMappingTeams method
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Token used for request cancellation.
+        /// </param>
+        public async System.Threading.Tasks.Task<IntegrationMappingTeams> CreateIntegrationMappingTeamsAsync(IntegrationMappingTeamsCreateRequest requestBody, CreateIntegrationMappingTeamsHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) {
+            headers = headers ?? new CreateIntegrationMappingTeamsHeaders();
+            Dictionary<string, string> headersMap = Utils.PrepareParams(map: DictionaryUtils.MergeDictionaries(new Dictionary<string, string?>() {  }, headers.ExtraHeaders));
+            FetchResponse response = await this.NetworkSession.NetworkClient.FetchAsync(options: new FetchOptions(url: string.Concat(this.NetworkSession.BaseUrls.BaseUrl, "/2.0/integration_mappings/teams"), method: "POST", contentType: "application/json", responseFormat: Box.Sdk.Gen.ResponseFormat.Json) { Headers = headersMap, Data = SimpleJsonSerializer.Serialize(requestBody), Auth = this.Auth, NetworkSession = this.NetworkSession, CancellationToken = cancellationToken }).ConfigureAwait(false);
+            return SimpleJsonSerializer.Deserialize<IntegrationMappingTeams>(NullableUtils.Unwrap(response.Data));
+        }
+
+        /// <summary>
+        /// Updates a [Teams integration mapping](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams).
+        /// Supports updating the Box folder ID and options.
+        /// You need Admin or Co-Admin role to
+        /// use this endpoint.
+        /// </summary>
+        /// <param name="integrationMappingId">
+        /// An ID of an integration mapping
+        /// Example: "11235432"
+        /// </param>
+        /// <param name="requestBody">
+        /// Request body of updateIntegrationMappingTeamsById method
+        /// </param>
+        /// <param name="headers">
+        /// Headers of updateIntegrationMappingTeamsById method
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Token used for request cancellation.
+        /// </param>
+        public async System.Threading.Tasks.Task<IntegrationMappingTeams> UpdateIntegrationMappingTeamsByIdAsync(string integrationMappingId, UpdateIntegrationMappingTeamsByIdRequestBody? requestBody = default, UpdateIntegrationMappingTeamsByIdHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) {
+            requestBody = requestBody ?? new UpdateIntegrationMappingTeamsByIdRequestBody();
+            headers = headers ?? new UpdateIntegrationMappingTeamsByIdHeaders();
+            Dictionary<string, string> headersMap = Utils.PrepareParams(map: DictionaryUtils.MergeDictionaries(new Dictionary<string, string?>() {  }, headers.ExtraHeaders));
+            FetchResponse response = await this.NetworkSession.NetworkClient.FetchAsync(options: new FetchOptions(url: string.Concat(this.NetworkSession.BaseUrls.BaseUrl, "/2.0/integration_mappings/teams/", StringUtils.ToStringRepresentation(integrationMappingId)), method: "PUT", contentType: "application/json", responseFormat: Box.Sdk.Gen.ResponseFormat.Json) { Headers = headersMap, Data = SimpleJsonSerializer.Serialize(requestBody), Auth = this.Auth, NetworkSession = this.NetworkSession, CancellationToken = cancellationToken }).ConfigureAwait(false);
+            return SimpleJsonSerializer.Deserialize<IntegrationMappingTeams>(NullableUtils.Unwrap(response.Data));
+        }
+
+        /// <summary>
+        /// Deletes a [Teams integration mapping](https://support.box.com/hc/en-us/articles/360044681474-Using-Box-for-Teams).
+        /// You need Admin or Co-Admin role to
+        /// use this endpoint.
+        /// </summary>
+        /// <param name="integrationMappingId">
+        /// An ID of an integration mapping
+        /// Example: "11235432"
+        /// </param>
+        /// <param name="headers">
+        /// Headers of deleteIntegrationMappingTeamsById method
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Token used for request cancellation.
+        /// </param>
+        public async System.Threading.Tasks.Task DeleteIntegrationMappingTeamsByIdAsync(string integrationMappingId, DeleteIntegrationMappingTeamsByIdHeaders? headers = default, System.Threading.CancellationToken? cancellationToken = null) {
+            headers = headers ?? new DeleteIntegrationMappingTeamsByIdHeaders();
+            Dictionary<string, string> headersMap = Utils.PrepareParams(map: DictionaryUtils.MergeDictionaries(new Dictionary<string, string?>() {  }, headers.ExtraHeaders));
+            FetchResponse response = await this.NetworkSession.NetworkClient.FetchAsync(options: new FetchOptions(url: string.Concat(this.NetworkSession.BaseUrls.BaseUrl, "/2.0/integration_mappings/teams/", StringUtils.ToStringRepresentation(integrationMappingId)), method: "DELETE", responseFormat: Box.Sdk.Gen.ResponseFormat.NoContent) { Headers = headersMap, Auth = this.Auth, NetworkSession = this.NetworkSession, CancellationToken = cancellationToken }).ConfigureAwait(false);
+        }
+
     }
 }
