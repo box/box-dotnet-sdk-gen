bec855f2d9ba24f89df5669aa21b231436082ec3
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-03-12 15:54:24 +0100

    chore: add net standard support for `OneOfs` (box/box-codegen#674) (#421)

--- Box.Sdk.Gen/Internal/OneOfT2.cs
+++ Box.Sdk.Gen/Internal/OneOfT2.cs
@@ -1,8 +1,16 @@
 namespace Box.Sdk.Gen {
     public class OneOf<T0,T1> {
+#if NETSTANDARD2_0
+        protected T0 _val0 { get; }
+#else
         protected T0? _val0 { get; }
+#endif
         
+#if NETSTANDARD2_0
+        protected T1 _val1 { get; }
+#else
         protected T1? _val1 { get; }
+#endif
         
         protected OneOf (T0 value) { _val0 = value; }
         
--- Box.Sdk.Gen/Internal/OneOfT4.cs
+++ Box.Sdk.Gen/Internal/OneOfT4.cs
@@ -1,12 +1,28 @@
 namespace Box.Sdk.Gen {
     public class OneOf<T0,T1,T2,T3> {
+#if NETSTANDARD2_0
+        protected T0 _val0 { get; }
+#else
         protected T0? _val0 { get; }
+#endif
         
+#if NETSTANDARD2_0
+        protected T1 _val1 { get; }
+#else
         protected T1? _val1 { get; }
+#endif
         
+#if NETSTANDARD2_0
+        protected T2 _val2 { get; }
+#else
         protected T2? _val2 { get; }
+#endif
         
+#if NETSTANDARD2_0
+        protected T3 _val3 { get; }
+#else
         protected T3? _val3 { get; }
+#endif
         
         protected OneOf (T0 value) { _val0 = value; }
         
