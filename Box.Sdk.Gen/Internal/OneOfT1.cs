633453f243d7652240ac7d5fecad2798898f3942
box-sdk-build <sdk-build@box.com>
2025-07-25 02:12:23 -0700

    feat: use composition instead of inheritance in C# unions (box/box-codegen#768)

--- Box.Sdk.Gen/Internal/OneOfT1.cs
+++ Box.Sdk.Gen/Internal/OneOfT1.cs
@@ -1,12 +1,12 @@
 namespace Box.Sdk.Gen {
     public class OneOf<T0> {
 #if NETSTANDARD2_0
-        protected T0 _val0 { get; }
+        public T0 _val0 { get; }
 #else
-        protected T0? _val0 { get; }
+        public T0? _val0 { get; }
 #endif
         
-        protected OneOf (T0 value) { _val0 = value; }
+        public OneOf (T0 value) { _val0 = value; }
         
         public static implicit operator OneOf<T0>(T0 value) => new OneOf<T0>(value);
     }
