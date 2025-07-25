fce68210d16c3eb81dbe907182126f54e2d8610c
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-07-25 14:05:14 +0200

    feat: Use composition instead of inheritance in unions (box/box-codegen#768) (#544)
    Co-authored-by: box-sdk-build <box-sdk-build@box.com>

--- Box.Sdk.Gen/Internal/OneOfT2.cs
+++ Box.Sdk.Gen/Internal/OneOfT2.cs
@@ -1,20 +1,20 @@
 namespace Box.Sdk.Gen {
     public class OneOf<T0,T1> {
 #if NETSTANDARD2_0
-        protected T0 _val0 { get; }
+        public T0 _val0 { get; }
 #else
-        protected T0? _val0 { get; }
+        public T0? _val0 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T1 _val1 { get; }
+        public T1 _val1 { get; }
 #else
-        protected T1? _val1 { get; }
+        public T1? _val1 { get; }
 #endif
         
-        protected OneOf (T0 value) { _val0 = value; }
+        public OneOf (T0 value) { _val0 = value; }
         
-        protected OneOf (T1 value) { _val1 = value; }
+        public OneOf (T1 value) { _val1 = value; }
         
         public static implicit operator OneOf<T0,T1>(T0 value) => new OneOf<T0,T1>(value);
         
--- Box.Sdk.Gen/Internal/OneOfT7.cs
+++ Box.Sdk.Gen/Internal/OneOfT7.cs
@@ -1,60 +1,60 @@
 namespace Box.Sdk.Gen {
     public class OneOf<T0,T1,T2,T3,T4,T5,T6> {
 #if NETSTANDARD2_0
-        protected T0 _val0 { get; }
+        public T0 _val0 { get; }
 #else
-        protected T0? _val0 { get; }
+        public T0? _val0 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T1 _val1 { get; }
+        public T1 _val1 { get; }
 #else
-        protected T1? _val1 { get; }
+        public T1? _val1 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T2 _val2 { get; }
+        public T2 _val2 { get; }
 #else
-        protected T2? _val2 { get; }
+        public T2? _val2 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T3 _val3 { get; }
+        public T3 _val3 { get; }
 #else
-        protected T3? _val3 { get; }
+        public T3? _val3 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T4 _val4 { get; }
+        public T4 _val4 { get; }
 #else
-        protected T4? _val4 { get; }
+        public T4? _val4 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T5 _val5 { get; }
+        public T5 _val5 { get; }
 #else
-        protected T5? _val5 { get; }
+        public T5? _val5 { get; }
 #endif
         
 #if NETSTANDARD2_0
-        protected T6 _val6 { get; }
+        public T6 _val6 { get; }
 #else
-        protected T6? _val6 { get; }
+        public T6? _val6 { get; }
 #endif
         
-        protected OneOf (T0 value) { _val0 = value; }
+        public OneOf (T0 value) { _val0 = value; }
         
-        protected OneOf (T1 value) { _val1 = value; }
+        public OneOf (T1 value) { _val1 = value; }
         
-        protected OneOf (T2 value) { _val2 = value; }
+        public OneOf (T2 value) { _val2 = value; }
         
-        protected OneOf (T3 value) { _val3 = value; }
+        public OneOf (T3 value) { _val3 = value; }
         
-        protected OneOf (T4 value) { _val4 = value; }
+        public OneOf (T4 value) { _val4 = value; }
         
-        protected OneOf (T5 value) { _val5 = value; }
+        public OneOf (T5 value) { _val5 = value; }
         
-        protected OneOf (T6 value) { _val6 = value; }
+        public OneOf (T6 value) { _val6 = value; }
         
         public static implicit operator OneOf<T0,T1,T2,T3,T4,T5,T6>(T0 value) => new OneOf<T0,T1,T2,T3,T4,T5,T6>(value);
         
